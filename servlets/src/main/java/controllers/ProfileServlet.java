1698617e022314a3debb26ee71d301c12ff3ca9d
Alexander Shakhov <write.me.messages@gmail.com>
2016-11-16 21:44:34 +0300

    Friends

--- servlets/src/main/java/controllers/ProfileServlet.java
+++ servlets/src/main/java/controllers/ProfileServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 /**
  * Created by Aleksandr_Shakhov on 13.11.16 19:45.
